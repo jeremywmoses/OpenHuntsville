d95ea2bfd2600296985b44693da53f7f7e44d656
Tyler Hughes <thughes@randomlyknighted.com>
2017-03-20 17:47:07 -0500

    WIP - removing needed CSS and JS files

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,5 @@
 $(document).ready(function() {
-	$(".fancybox").fancybox({
-		width	: 600,
-		helpers : {
-			title : {
-				type : 'over'
-			},
-			overlay : {
-				css : {
-					'background' : 'rgba(0, 0, 0, 0.7)'
-				}
-			}
-		}
-	});
-	$(".feedback").fancybox({
-		maxWidth	: 610,
-		maxHeight	: 600,
-		helpers : {
-			overlay : {
-				css : {
-					'background' : 'rgba(0, 0, 0, 0.7)'
-				}
-			}
-		}
-	});
-	$(function () {
-		if ($('.parentcats').length)
-		{
-			$('.parentcats').tinyNav({
-				active: 'selected'
-			});
-		}
-	});
+
 	$('html').addClass('js');
 	$(function() {
 		if ($('.parentcats').length)
